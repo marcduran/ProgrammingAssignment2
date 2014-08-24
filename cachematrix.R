8a7553c241083cab0d51ac6bab2671017b03b915
marcduran <marcduran@gmx.de>
2014-08-25 01:31:14 +0200

    late

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I don't really understand what this program does.
+## Although I was able to reproduce the expected behavior I don't
+## think I deserve credit for what I did. I just adapted the code
+## provided as an example for vectors in order to make it work for 
+## matrices. 
 
-## Write a short comment describing this function
+## inv is the result of inverting the matrix with the function
+## 'solve'. The factors 'setinv' and 'getinv' are just
+## 'setmean' and 'getmean' renamed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
